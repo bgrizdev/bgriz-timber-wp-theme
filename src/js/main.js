7425f0f11af76f52559ade81e2d777205903df9b
Ben G <grzybowski1911@gmail.com>
2024-10-31 07:33:06 -0700

    remove buggy menu js

--- src/js/main.js
+++ src/js/main.js
@@ -24,88 +24,10 @@ const fadeLogo = () => {
     }
 };
 
-const menuOpen = () => {
-    const mobileToggle = document.getElementById('mobile-menu-toggle');
-
-    const mainMenu = document.querySelector('.main-menu');
-
-    mobileToggle.addEventListener('click', () => {
-        console.log('click');
-        mainMenu.addClass('main-menu-open');
-    });
-}
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    menuOpen();
-
     window.addEventListener('scroll', fadeLogo);
-
-    var menuItems = document.querySelectorAll('.main-menu > li');
-
-    function setMenuChildrenWidth() {
-
-        console.log('set menu width');
-
-        var navContainer = document.querySelector('.nav-container');
-
-        menuItems.forEach(function(menuItem) {
-            var menuChildren = menuItem.querySelector('.menu-children');
-
-            if (menuChildren && navContainer) {
-                menuChildren.style.width = navContainer.offsetWidth + 'px';
-            }
-        });
-    }
-    setMenuChildrenWidth();
-
-    window.addEventListener('resize', setMenuChildrenWidth);
-
-    var menuItems = document.querySelectorAll('.main-menu > li');
-
-    menuItems.forEach(function(menuItem) {
-        var menuChildren = menuItem.querySelector('.menu-children');
-
-        menuItem.addEventListener('mouseover', function() {
-            if (menuChildren) {
-                menuChildren.style.display = 'block';
-            }
-        });
-
-        menuItem.addEventListener('mouseout', function(event) {
-            if (!isCursorWithinElement(event, menuItem) && (!menuChildren || !isCursorWithinElement(event, menuChildren))) {
-                if (menuChildren) {
-                    menuChildren.style.display = 'none';
-                }
-            }
-        });
-
-        if (menuChildren) {
-            menuChildren.addEventListener('mouseover', function() {
-                menuChildren.style.display = 'block';
-            });
-
-            menuChildren.addEventListener('mouseout', function(event) {
-                if (!isCursorWithinElement(event, menuItem) && !isCursorWithinElement(event, menuChildren)) {
-                    menuChildren.style.display = 'none';
-                }
-            });
-        }
-    });
-
-    function isCursorWithinElement(event, element) {
-        if (!element) {
-            return false;
-        }
-        
-        var rect = element.getBoundingClientRect();
-        return (
-            event.clientX >= rect.left &&
-            event.clientX <= rect.right &&
-            event.clientY >= rect.top &&
-            event.clientY <= rect.bottom
-        );
-    }
 
     let lastScrollTop = 0;
     const header = document.querySelector('.header');
